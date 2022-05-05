f6311b30aa602311e75c420eb37e4be0d55077b8
Hamid <www.abdulhamid3311@gmail.com>
2022-05-05 14:41:02 +0600

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Footer</h2>
-        </div>
+        <footer className='footer-container text-center text-white mt-3 pt-5'>
+            <h2>Dream Bike</h2>
+            <p>All Right Reserved &copy; Dream Bike | 2022</p>
+
+        </footer>
     );
 };
 
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Services = () => {
     return (
         <div>
             <h1 className='text-center text-primary my-5'> services</h1>
             <div className='service-card'>
-                
+
+                <FontAwesomeIcon icon={faCoffee} size='lg' />
 
             </div>
         </div>
